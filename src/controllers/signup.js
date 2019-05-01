a65ad1bf55bd1c98981f7cbaab948db14ac2f91f
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-05-01 14:00:32 +0100

    Verying that user hasn't registered befor

--- src/controllers/signup.js
+++ src/controllers/signup.js
@@ -1,14 +1,31 @@
+import bcrypt from 'bcrypt';
 import pool from '../models/db';
 
 const signUp = (req, res, next) => {
-  const { username, password} = req.body;
-  const query = `INSERT INTO users (username, password, registered) VALUES ('${username}', '${password}', NOW());`;
-  pool
-    .query(query)
-    .then(r => console.log(r))
-    .catch(e => console.log(e.stack));
+  const { username, password } = req.body;
 
-  res.status(200).json({ message: 'Please sign up' });
+  pool
+    .query(`SELECT * FROM USERS where username = '${username}';`)
+    .then((r) => {
+      // check if mail exists before signing in
+      if (r.rows[0]) {
+        return res.status(409).json({
+          status: 409,
+          message: 'Error Signing up. Username Already Exists',
+        });
+      }
+
+      // register the new user
+      const query = `INSERT INTO users (username, password, registered) VALUES ('${username}', '${password}', NOW());`;
+      pool
+        .query(query)
+        .then(r => {
+          res.status(200).json({ message: 'Successfully created a new account' });
+        })
+        .catch(e => console.log(e.stack));
+    });
+
+
 };
 
 export default signUp;
