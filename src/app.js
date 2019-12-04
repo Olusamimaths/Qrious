1475770fc68752be8c44b0e99262cfa6de93e776
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-12-04 11:41:17 +0100

    Cors again

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const app = express();
 
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
 });
 
