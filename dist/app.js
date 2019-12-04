e0d8f9e6e91d173ccfa620a164c329ec2f048208
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-12-04 12:13:20 +0100

    This should work now

--- dist/app.js
+++ dist/app.js
@@ -25,7 +25,7 @@ _dotenv.default.config(); // initialize the app
 
 const app = (0, _express.default)();
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://olusamimaths.github.io/");
+  res.header("Access-Control-Allow-Origin", "https://olusamimaths.github.io");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Credentials");
   next();
