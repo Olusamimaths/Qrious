82fcc20112f9bd895c9cb1388e16abb4de8e8bda
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-12-04 12:16:56 +0100

    This should work now

--- dist/app.js
+++ dist/app.js
@@ -27,6 +27,7 @@ const app = (0, _express.default)();
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "https://olusamimaths.github.io");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header('Access-Control-Allow-Credentials', 'true');
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Credentials");
   next();
 }); // setup body parser
