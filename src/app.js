c3674c1bc420850414849855ab1e885add770e44
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2019-12-04 12:06:13 +0100

    This should work now

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ const app = express();
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Credentials");
   next();
 });
 
