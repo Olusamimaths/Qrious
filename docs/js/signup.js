e6664d1de3392fe1ab11e4624d80b7077d6cfd21
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2022-01-17 08:20:47 +0100

    :fire: Password fix

--- docs/js/signup.js
+++ docs/js/signup.js
@@ -11,8 +11,8 @@ window.onload = () => {
     fetch(url, {
       method: 'POST',
       body: JSON.stringify(data),
-      credentials: 'include',
-      mode: 'cors',
+      // credentials: 'include',
+      // mode: 'cors',
       headers: {
         Accept: 'application/json, text/plain, */*',
         'Content-type': 'application/json',
--- src/app.js
+++ src/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 const app = express();
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "https://olusamimaths.github.io");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header('Access-Control-Allow-Credentials', 'true')
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization, Credentials");
