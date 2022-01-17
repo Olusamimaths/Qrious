573b7b04eaf39745f7e2f572159464f87a7611e2
Olusamimaths <olusola.samuel.oluwatobi@gmail.com>
2022-01-17 07:50:16 +0100

    :fire: Password fix

--- src/helper/signup.validator.js
+++ src/helper/signup.validator.js
@@ -2,7 +2,8 @@ import Joi from 'joi';
 
 const schema = Joi.object().keys({
   username: Joi.string().alphanum().min(3).max(30).required(),
-  password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  // password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/).required(),
+  password: Joi.string().required(),
 });
 // joi schema
 const validate = (username, password) => Joi.validate(
